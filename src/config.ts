876b499d0deb1e88b1d0da76e66ccc6e0207116f
hanbz <hanbzsky@yahoo.com.tw>
2023-04-19 21:56:22 +0800

    feat:add the mingshaotsai config

--- src/config.ts
+++ src/config.ts
@@ -22,16 +22,32 @@ export const defaultConfig: IRoutes = {
 
 export const configRoutes = new Map<string, IRoutes>([
   [
-    '/test',
+    '/mingshaotsai',
     {
-      title: 'Medify-test',
-      subTitle: '官方網站',
-      logoLink: logoPng,
+      title: '耳鼻喉系口腔咽喉科',
+      subTitle: '蔡明劭醫師',
+      logoLink: 'https://medify-picture.s3.ap-southeast-1.amazonaws.com/avatars/25HOwlHlEMeXQFxOGgMSNOqnvEl1a3KXHIsbbNYN.jpg',
       options: [
         {
-          name: '官方網站',
-          url: 'https://google.com',
+          name: 'MEDIFY系統連結',
+          url: 'https://medify.app/doctor/10?utm_source=10&utm_medium=integrate_page',
         },
+        {
+          name: '蔡醫師blog',
+          url: 'http://mingshaotsaimd.blogspot.com/2018/01/blog-post.html',
+        },
+        {
+          name: '蔡醫師臉書專頁',
+          url: 'https://www.facebook.com/ENTDRTSAI',
+        },
+        {
+          name: '長庚官網',
+          url: 'https://www.cgmh.org.tw/tw/Services/DoctorInfo/8031',
+        },
+        {
+          name: '長庚掛號系統',
+          url: 'https://register.cgmh.org.tw/Department/6/63500A?id=8031&name=%E8%94%A1%E6%98%8E%E5%8A%AD',
+        }
       ],
     },
   ],
